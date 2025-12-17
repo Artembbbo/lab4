2f93f79c803175e20d368993ebe032be791aa3c2
Artem <artikboltut@gmail.com>
2025-12-17 21:34:22 +0300

    задание 2.cpp

--- задание 2.cpp
+++ задание 2.cpp
@@ -1,10 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cstdlib>
 int** make(int, int);
-void enter_elements(int, int**,int,int);
-void find_max(int, int**);
-int sum_triangle(int, int**);
-void clear_array(int, int**);
-void output_matrix(int, int**);
+void enter_elements(int, int**&, int, int);
+void find_max(int, int**&);
+int sum_triangle(int, int**&);
+void clear_array(int, int**&);
+void output_matrix(int, int**&);
 int main()
 {
 	int n = 0;
@@ -19,7 +20,7 @@ int main()
 		int col = n;
 		make(row, col);
 		int** matrix = make(row, col);
-		enter_elements(n, matrix,a,b);
+		enter_elements(n, matrix, a, b);
 		output_matrix(n, matrix);
 		find_max(n, matrix);
 		std::cout << "sum in triangle" << " " << sum_triangle(n, matrix);
@@ -39,15 +40,15 @@ int** make(int row, int col)
 	}
 	return matrix;
 }
-void enter_elements(int n, int** matrix,int a,int b) {
+void enter_elements(int n, int**& matrix, int a, int b) {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			matrix[i][j] = std::rand()% (b - a + 1) + a;
+			matrix[i][j] = std::rand() % (b - a + 1) + a;
 		}
 	}
 }
-void find_max(int n, int** matrix) {
-	int max = -32678;
+void find_max(int n, int**& matrix) {
+	int max = INT32_MIN;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			if (matrix[j][i] > max) {
@@ -58,10 +59,10 @@ void find_max(int n, int** matrix) {
 			std::cout << "max element in neg col" << i + 1 << " " << max << " ";
 		}
 		else std::cout << "in col" << " " << i + 1 << " " << "positive element" << " ";
-		max = -32768;
+		max = INT32_MIN;
 	}
 }
-int sum_triangle(int n, int** matrix) {
+int sum_triangle(int n, int**& matrix) {
 	int sum = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0 + i; j < n; j++) {
@@ -73,17 +74,18 @@ int sum_triangle(int n, int** matrix) {
 	}
 	return sum;
 }
-void clear_array(int n, int** matrix) {
+void clear_array(int n, int**& matrix) {
 	for (int i = 0; i < n; i++) {
 		delete[] matrix[i];
 	}
 	delete[] matrix;
 }
-void output_matrix(int n, int** matrix) {
+void output_matrix(int n, int**& matrix) {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			std::cout << matrix[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
+
 }
